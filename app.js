853dda09dc450e1af8d626e929ca48663be121fb
atharv0811 <karnekaratharv12@gmail.com>
2023-10-05 14:42:21 +0530

    some minor changes

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-items');
+const port = 3000;
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -19,10 +20,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-    User.findByPk(1).then((user) => {
-        req.user = user;
-        next();
-    }).catch(err => console.log(err));
+    User.findByPk(1)
+        .then((user) => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
 })
 
 app.use('/admin', adminRoutes);
@@ -37,19 +40,25 @@ Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
 
-sequelize.sync().then(result => {
-    return User.findByPk(1);
-}).then(user => {
-    if (!user) {
-        return User.create({ name: 'Atharv', email: 'test@test.com' })
-    }
-    return user;
-}).then(user => {
-    return user.createCart();
-}).then(cart => {
-    app.listen(3000);
-}).catch(err => {
-    console.log(err)
-});
+sequelize.sync()
+    .then(result => {
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({ name: 'Atharv', email: 'test@test.com' })
+        }
+        return user;
+    })
+    .then(user => {
+        return user.createCart();
+    })
+    .then(cart => {
+        app.listen(port);
+        console.log(`Server is running on port: http://localhost:${port}`)
+    })
+    .catch(err => {
+        console.log(err)
+    });
 
 
--- controllers/shop.js
+++ controllers/shop.js
@@ -13,37 +13,40 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findAll({ where: { id: prodId } }).then(product => {
-    res.render('shop/product-detail', {
-      product: product[0],
-      pageTitle: product.title,
-      path: '/products'
+  Product.findAll({ where: { id: prodId } })
+    .then(product => {
+      res.render('shop/product-detail', {
+        product: product[0],
+        pageTitle: product.title,
+        path: '/products'
+      })
     })
-  }).catch(err => console.log(err))
+    .catch(err => console.log(err))
 }
 
 exports.getIndex = (req, res, next) => {
-  Product.findAll().then(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/'
-    });
-  }).catch(err => console.log(err));
-
+  Product.findAll()
+    .then(products => {
+      res.render('shop/index', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    }).catch(err => console.log(err));
 };
 
 exports.getCart = (req, res, next) => {
-  req.user.getCart().then(cart => {
-    return cart.getProducts()
-      .then(products => {
-        res.render('shop/cart', {
-          path: '/cart',
-          pageTitle: 'Your Cart',
-          products: products
-        });
-      }).catch(err => console.log(err));
-  }).catch(err => console.log(err))
+  req.user.getCart()
+    .then(cart => {
+      return cart.getProducts()
+        .then(products => {
+          res.render('shop/cart', {
+            path: '/cart',
+            pageTitle: 'Your Cart',
+            products: products
+          });
+        }).catch(err => console.log(err));
+    }).catch(err => console.log(err))
 };
 
 exports.postCart = (req, res, next) => {
@@ -93,12 +96,15 @@ exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user.getCart().then(cart => {
     return cart.getProducts({ where: { id: prodId } });
-  }).then((products) => {
-    const product = products[0];
-    return product.cartItem.destroy();
-  }).then(() => {
-    res.redirect('/cart');
-  }).catch(err => {
-    console.log(err);
-  });
+  })
+    .then((products) => {
+      const product = products[0];
+      return product.cartItem.destroy();
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
